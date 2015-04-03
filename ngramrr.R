20baf86138f359ccd420dfd942a99e437a67b47d
chainsawriot <chainsawtiney@gmail.com>
2015-04-03 17:52:53 +0800

    Using tau, seems to work somehow, but cannot use in the TermDocumentMatrix function

--- ngramrr.R
+++ ngramrr.R
@@ -1,5 +1,5 @@
 require(ngram)
-ngramrr <- function(x, ngmin = 1, ngmax = 2) {
+ngramrrx <- function(x, ngmin = 1, ngmax = 2) {
     if (ngmin >= ngmax) {
         stop("ngmin must be larger than ngmax")
     }
@@ -7,7 +7,40 @@ ngramrr <- function(x, ngmin = 1, ngmax = 2) {
     return(Reduce(c, Map(function(n) get.ngrams(ngram(y,n)), ngmin:ngmax )))
 }
 text <- c("A B C", "B C B C D", "A C D")
-trigram <- function(x) ngramrr(x, 1,3)
-tdm <- TermDocumentMatrix(Corpus(VectorSource(text)), control = list(tokenize = trigram))
+trigramx <- function(x) ngramrrx(x, 1,3)
+tdm <- TermDocumentMatrix(Corpus(VectorSource(text)), control = list(tokenize = trigramx))
 ## wrong the frequency is incorrect because ngram will not return frequency, shit
 MC_tokenizer(text)
+
+require(tau)
+taungram <- function(text, n = 1, tolower = FALSE,...) {
+    r <- textcnt(text, method = 'string', n = n, tolower = tolower, ...)
+    g <- (Reduce(c, sapply(1:length(r), function(x) rep(names(r[x]), r[x]))))
+    gsub(" ", "/", g)
+}
+
+ngramrr <- function(x, ngmin = 1, ngmax = 2) {
+    if (ngmin >= ngmax) {
+        stop("ngmin must be larger than ngmax")
+    }
+    y <- paste(x, " ")
+    return(Reduce(c, Map(function(n) taungram(x, n), ngmin:ngmax )))
+}
+ngramrrstupid <- function(x, ngmin = 1, ngmax = 2) {
+    if (ngmin >= ngmax) {
+        stop("ngmin must be larger than ngmax")
+    }
+    y <- paste(x, " ")
+    return(paste(Reduce(c, Map(function(n) taungram(x, n), ngmin:ngmax )), collapse = " "))
+}
+require(tm)
+text <- c("on9 無極限", "好 撚 on9 呀 你 on9 仔", "on9 仔 你 去 仆街 喇") # should not remove number
+trigram <- function(x) ngramrr(x, 1 , 3)
+#MC_tokenizer(c("A B C", "B C B C D", "A C D"))
+
+tdm <- TermDocumentMatrix(Corpus(VectorSource(iconv(text, to='utf-8'))), control = list(tokenize = trigram)) # don't know why it didn't work
+
+trigram <- function(x) ngramrrstupid(x, 1 , 3)
+stupid <- sapply(text, ngramrrstupid)
+
+stupid.tdm <- TermDocumentMatrix(Corpus(VectorSource(stupid)), control = list(wordLengths=c(1, Inf)))
