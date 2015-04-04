1c670fb56333206f2d51cca70d5cab2155d1185e
chainsawriot <chainsawtiney@gmail.com>
2015-04-04 21:13:46 +0800

    Added Char ngram and capping the ngmax when sentence length < ngmax

--- R/ngramrr.R
+++ R/ngramrr.R
@@ -1,17 +1,28 @@
-#require(tau)
-#require(tm)
-
 taungram <- function(text, n = 1, tolower = FALSE, split = "[[:space:]]+", ...) {
     r <- textcnt(text, method = 'string', n = n, tolower = tolower, split = split, ...)
-    #return(r)
-    g <- (Reduce(c, sapply(1:length(r), function(x) rep(names(r[x]), r[x]))))
-    #gsub(" ", "/", g)
+    return(Reduce(c, sapply(1:length(r), function(x) rep(names(r[x]), r[x]))))
 }
 
-ngramrr <- function(x, ngmin = 1, ngmax = 2) {
-    if (ngmin >= ngmax) {
-        stop("ngmin must be larger than ngmax")
+tauchar <- function(text, n = 1, tolower = FALSE, split = "[[:space:]]+", rmEOL = FALSE, ...) {
+    r <- textcnt(text, method = 'ngram', n = n, tolower = tolower, split = split, ...)
+    g <- unlist(sapply(1:length(r), function(x) rep(names(r[x]), r[x])))
+    if (rmEOL) {
+        g <- g[grep("_", g, invert = TRUE)]
     }
-    y <- paste(x, " ")
-    return(Reduce(c, Map(function(n) taungram(y, n), ngmin:ngmax )))
+    return(g)
+}
+ngramrr <- function(x, char = FALSE, ngmin = 1, ngmax = 2, rmEOL = TRUE) {
+    if (ngmin > ngmax) {
+        stop("ngmax must be higher than ngmin")
+    }
+    y <- paste(x, collapse = " ")
+    if (char) {
+        return(tauchar(x, n = ngmax, rmEOL = rmEOL))
+    }
+    sentencelength <- length(unlist(strsplit(y, split = " ")))
+    if (sentencelength > ngmax) {
+        return(Reduce(c, Map(function(n) taungram(y, n), seq(from = ngmin, to = ngmax))))
+    } else {
+        return(Reduce(c, Map(function(n) taungram(y, n), seq(from = ngmin, to = sentencelength ))))
+    }
 }
--- README.md
+++ README.md
@@ -6,7 +6,11 @@ A fast ngram tokenizer for R so that we don't need to install RWeka just for NGr
 ```{r}
 require(ngramrr)
 require(tm)
-nirvana <- c("hello hello hello how low", "hello hello hello how low", "hello hello hello how low", "hello hello hello", "with the lights out", "it's less dangerous", "here we are now", "entertain us", "i feel stupid", "and contagious", "here we are now", "entertain us", "a mulatto", "an albino", "a mosquito", "my libido", "yeah", "hey yay")
+nirvana <- c("hello hello hello how low", "hello hello hello how low",
+"hello hello hello how low", "hello hello hello",
+"with the lights out", "it's less dangerous", "here we are now", "entertain us",
+"i feel stupid", "and contagious", "here we are now", "entertain us",
+"a mulatto", "an albino", "a mosquito", "my libido", "yeah", "hey yay")
 
 ngramrr(nirvana[1], ngmax = 3)
 tdm <- TermDocumentMatrix(Corpus(VectorSource(nirvana)), control = list(tokenize = function(x) ngramrr(x, ngmin = 1, ngmax=2)))
